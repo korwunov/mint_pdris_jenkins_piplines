ccb7900643508a0e717f6702c77230194c1fcdf7
p.korshunov <korshunov.petr@gmail.com>
2025-12-04 15:34:31 +0300

    fix deploy

--- deployJenkinsfile.groovy
+++ deployJenkinsfile.groovy
@@ -5,21 +5,23 @@ pipeline {
         string(name: 'nexusAtrifactUrl', defaultValue: '', description: 'Url to get artifact from nexus')
     }
 
-    stage('Download from Nexus') {
-        environment {
-            NEXUS_URL = 'nexus:8081'
-            NEXUS_CREDS = credentials('nexus_user_password')
-        }
-        steps {
-            sh "wget --user=${NEXUS_CREDS_USR} --password=${NEXUS_CREDS_PSW} -O distr.jar ${params.nexusAtrifactUrl}"
-            sh "ls"
-        }
+    stages {
+        stage('Download from Nexus') {
+            environment {
+                NEXUS_URL = 'nexus:8081'
+                NEXUS_CREDS = credentials('nexus_user_password')
+            }
+            steps {
+                sh "wget --user=${NEXUS_CREDS_USR} --password=${NEXUS_CREDS_PSW} -O distr.jar ${params.nexusAtrifactUrl}"
+                sh "ls"
+            }
         
-    }
+        }
     
-    stage('Deploy') {
-        steps {
-            print('deploy stage here')
+        stage('Deploy') {
+            steps {
+                print('deploy stage here')
+            }
         }
     }
 
